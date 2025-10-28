20bf32266a443e18cc74e2bfff3fb69fbab71ff2
Victor Henrique de Paula Ferreira <victorhpf97@gmail.com>
2025-10-28 18:52:27 +0000

    Adiciona rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App,
+        pathMatch: 'full'
+    }
+];
